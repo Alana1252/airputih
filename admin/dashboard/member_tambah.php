d53fd60a4e8efe518b40f1b98edbb877f65dba9d
Alana1252 <alankamesta.ginting@gmail.com>
2023-05-16 22:08:58 +0700

    polosan

--- admin/dashboard/member_tambah.php
+++ admin/dashboard/member_tambah.php
@@ -1,12 +1,7 @@
 <?php
 // Connect to database
 include_once '../../koneksi.php';
-
-// Get member data by ID
-$id = $_GET['id'];
-$sql = "SELECT * FROM member WHERE id='$id'";
-$result = $conn->query($sql);
-$row = $result->fetch_assoc();
+include_once 'css/script.php';
 
 // Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -24,125 +19,138 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // Upload photo
   $target_dir = "../../images/img-member/";
   $target_file = $target_dir . basename($_FILES["photo"]["name"]);
-  $uploadOk = 1;
-  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-  // Check if image file is a actual image or fake image
-  if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["photo"]["tmp_name"]);
-    if($check !== false) {
-      $uploadOk = 1;
-    } else {
-      echo "File is not an image.";
-      $uploadOk = 0;
-    }
-  }
- 
-  // Check file size
-  if ($_FILES["photo"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-  }
-  // Allow certain file formats
-  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-  && $imageFileType != "webm" ) {
-    echo "Sorry, only JPG, JPEG, & PNG files are allowed.";
-    $uploadOk = 0;
-  }
-  // Check if $uploadOk is set to 0 by an error
-  if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-  // if everything is ok, try to upload file
-  } else {
-    if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {
-      // Update member data
-      $sql = "UPDATE member
-      SET nama = '$nama', asal = '$asal', umur = '$umur', gender = '$gender', hobi = '$hobi', jabatan = '$jabatan', discord = '$discord', username = '$username', anggota = '$anggota', photo = '".basename($_FILES["photo"]["name"])."'
-      WHERE id = '$id';";
-      $conn->query($sql);
-
+  
+  // Move uploaded file to target directory
+  if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {
+    // Insert member data into database
+    $sql = "INSERT INTO member (nama, asal, umur, gender, hobi, jabatan, discord, username, anggota, photo) VALUES ('$nama', '$asal', '$umur', '$gender', '$hobi', '$jabatan', '$discord', '$username', '$anggota', '".basename($_FILES["photo"]["name"])."')";
+    if ($conn->query($sql) === TRUE) {
       // Redirect to member list page
       header("Location: member.php");
       exit();
     } else {
-      echo "Sorry, there was an error uploading your file.";
+      echo "Error: " . $sql . "<br>" . $conn->error;
     }
+  } else {
+    echo "Sorry, there was an error uploading your file.";
   }
 }
 
 // Close database connection
 $conn->close();
 ?>
-<!DOCTYPE html>
-<html>
+
+
+
+<!-- HTML form with Bootstrap styling -->
+    <html lang="en">
+
 <head>
-  <title>Contoh Form Upload</title>
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <!-- jQuery -->
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-  <!-- Bootstrap JS -->
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
-<body>
-  <div class="container">
-    <h2>Form Upload</h2>
-    <!-- Form upload dan elemen pesan -->
-    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
-      <!-- Form input fields -->
-      <div class="form-group">
-        <label for="nama">Nama:</label>
-        <input type="text" class="form-control" id="nama" name="nama">
-      </div>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-      <!-- ... -->
-
-      <!-- File input field -->
-      <div class="form-group">
-        <label for="photo">Photo:</label>
-        <input type="file" class="form-control" id="photo" name="photo">
-      </div>
-
-      <!-- Submit button -->
-      <button type="submit" class="btn btn-primary" name="submit">Upload</button>
-    </form>
-
-    <!-- Elemen pesan -->
-    <div id="message" class="alert alert-dismissible fade show" style="position: fixed; top: 20px; right: 20px; display: none;">
-      <button type="button" class="close" data-dismiss="alert">&times;</button>
-      <span id="message-text"></span>
+    <!-- Bootstrap -->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+    <!-- Data Tables -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+    <!-- Font Google -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+    <!-- Own CSS -->
+    <link rel="stylesheet" href="css/style.css">
+<form method="post" enctype="multipart/form-data" class="align-content-xl-center">
+<!---ATAS-->
+<?php include_once '../navbar.php'; ?>
+<div class="form-row ml-5 mt-1 justify-content-center">
+    <div class="col-md-2 mb-3 my-1">
+      <label for="nama">Nama :</label>
+      <input type="text" class="form-control" id="nama" name="nama" required placeholder="Masukan nama...">
+    </div>
+	  <div class="col-md-2 mb-3 my-1 ml-2">
+      <label for="username">Username :</label>
+      <input type="text" class="form-control" id="username" placeholder="Nama didalam game..." name="username" required>
+    </div>
+    <div class="col-md-2 mb-3 my-1 ml-2">
+      <label for="discord">Discrod tag :</label>
+      <input type="text" class="form-control" id="discord" placeholder="Velskude#6969" name="discord" required>
     </div>
   </div>
+<!-- Tengah -->
+<div class="form-row ml-5 mt-1 justify-content-center">
+    <div class="col-md-2 mb-3 my-1">
+      <label for="asal">Asal :</label>
+      <input type="text" class="form-control" id="asal" placeholder="Masukan asal daerah..." name="asal" required>
+    </div>
+    <div class="col-md-2 mb-3 my-1 ml-2">
+      <label for="umur">Umur :</label>
+      <input type="number" class="form-control" id="umur" placeholder="Masukan Umur..." name="umur" required>
+    </div>
+    <div class="col-md-2 mb-3 my-1 ml-2">
+      <label for="hobi">Hobi :</label>
+      <input type="text" class="form-control" id="hobi" placeholder="Masukan Hobi..." name="hobi" required>
+    </div>
+  </div>
+<!-- Bawah -->
+<div class="form-row ml-5 mt-1 justify-content-center">
+  <div class="col-md-2 mb-3">
+    <label class="sr-only" for="gender">Gender</label>
+	<div class="input-group">
+        <div class="input-group-prepend">
+        <div class="input-group-text">Gender</div>
+        </div>
+</div>
+	<select class="custom-select mr-sm-5" id="gender" name="gender">
+        <option selected>Choose</option>
+        <option value="Laki-Laki">Laki-Laki</option>
+      	<option value="Perempuan">Perempuan</option>
+      </select>
+		</div> 
 
-  <!-- Script untuk menampilkan pesan fade in dan fade out -->
-  <script>
-    $(document).ready(function() {
-      // Fade in pesan
-      function showMessage(message, className) {
-        $('#message-text').text(message);
-        $('#message').addClass(className).fadeIn();
+  <div class="col-md-2 mb-3 ml-2">
+    <label class="sr-only" for="jabatan">Jabatan</label>
+	<div class="input-group">
+        <div class="input-group-prepend">
+        <div class="input-group-text">Jabatan</div>
+        </div>
+	</div>
+	<select class="custom-select mr-sm-5" id="jabatan" name="jabatan">
+        <option selected>Choose</option>
+        <option value="Staff">Staff</option>
+        <option value="Donatur">Donatur</option>
+        <option value="Veteran">Veteran</option>
+        <option value="Ameteur">Ameteur</option>
+        <option value="Novice">Novice</option>
+      </select>
+	</div>
+  
+    <div class="col-md-2 mb-3 ml-2">
+    <label class="sr-only" for="anggota">Anggota</label>
+	<div class="input-group">
+        <div class="input-group-prepend">
+        <div class="input-group-text">Anggota</div>
+        </div>
+	</div>
+	<select class="custom-select mr-sm-5" id="anggota" name="anggota">
+        <option selected>Choose</option>
+        <option value="iya">iya</option>
+        <option value="tidak">tidak</option>
+      </select>
+	</div> 
+</div>
+ <div class="form-row row justify-content-center">
+    <div class="form-group col-md-4 ml-2 mr-1 mt-2">
+      <label for="photo">Photo:</label>
+      <input type="file" class="form-control-file" id="photo" name="photo" onchange="uploadFile(this)">
+  <progress class="upload-progress col-md-5 ml-1" id="progressbar" value="0" max="100"></progress>
+  <span id="progress-label">0%</span>
+    </div>
 
-        // Menghilangkan pesan setelah 5 detik
-        setTimeout(function() {
-          $('#message').fadeOut();
-        }, 5000);
-      }
-
-      // Validasi file size dan format
-      $('#photo').change(function() {
-        var file = this.files[0];
-        var fileSize = file.size;
-        var fileType = file.type;
-        var validFormats = ['image/jpeg', 'image/jpg', 'image/png'];
-
-        if (fileSize > 500000) {
-          showMessage('Sorry, your file is too large.', 'alert-danger');
-          $(this).val('');
-        } else if (!validFormats.includes(fileType)) {
-          showMessage('Sorry, only JPG, JPEG, and PNG files are allowed.', 'alert-danger');
-          $(this).val('');
-        }
-      });
-    });
-  </script>
-</body>
-</html>
+  <div class="form-group col-md-1 ml-5 mt-4">
+  <button type="submit" class="btn btn-primary">Submit</button>
+</div>
+</div>
+</form>
