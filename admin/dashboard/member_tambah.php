fad15c440062c8dbbcf3097a98fc779f552bcf92
Alana1252 <alankamesta.ginting@gmail.com>
2023-05-16 21:41:40 +0700

    commit

--- admin/dashboard/member_tambah.php
+++ admin/dashboard/member_tambah.php
@@ -34,29 +34,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
  
   // Check file size
   if ($_FILES["photo"]["size"] > 500000) {
-    echo "<script>
-      $(document).ready(function() {
-        $('#message').addClass('alert-danger').text('Sorry, your file is too large.').fadeIn().delay(3000).fadeOut();
-      });
-    </script>";
+    echo "Sorry, your file is too large.";
     $uploadOk = 0;
   }
   // Allow certain file formats
   if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-    echo "<script>
-      $(document).ready(function() {
-        $('#message').addClass('alert-danger').text('Sorry, only JPG, JPEG, PNG & GIF files are allowed.').fadeIn().delay(3000).fadeOut();
-      });
-    </script>";
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
   }
   // Check if $uploadOk is set to 0 by an error
   if ($uploadOk == 0) {
-    echo "<script>
-      $(document).ready(function() {
-        $('#message').addClass('alert-danger').text('Sorry, your file was not uploaded.').fadeIn().delay(3000).fadeOut();
-      });
-    </script>";
+    echo "Sorry, your file was not uploaded.";
   } else {
     // if everything is ok, try to upload file
     if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {
@@ -64,28 +52,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       $sql = "INSERT INTO member (nama, asal, umur, gender, hobi, jabatan, discord, username, anggota, photo) VALUES ('$nama', '$asal', '$umur', '$gender', '$hobi', '$jabatan', '$discord', '$username', '$anggota', '".basename($_FILES["photo"]["name"])."')";
       if ($conn->query($sql) === TRUE) {
         // Redirect to member list page
-        echo "<script>
-          $(document).ready(function() {
-            $('#message').addClass('alert-success').text('Data berhasil diupload.').fadeIn().delay(3000).fadeOut();
-});
-</script>";
-header("Location: member.php");
-exit();
-} else {
-echo "<script>
-$(document).ready(function() {
-$('#message').addClass('alert-danger').text('Error: " . $sql . "<br>" . $conn->error . "').fadeIn().delay(3000).fadeOut();
-});
-</script>";
-}
-} else {
-echo "<script>
-$(document).ready(function() {
-$('#message').addClass('alert-danger').text('Sorry, there was an error uploading your file.').fadeIn().delay(3000).fadeOut();
-});
-</script>";
-}
-}
+        header("Location: member.php");
+        exit();
+      } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+    } else {
+      echo "Sorry, there was an error uploading your file.";
+    }
+  }
 }
 
 // Close database connection
@@ -97,43 +72,52 @@ $conn->close();
     <html lang="en">
 
 <head>
-   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <!-- jQuery -->
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-  <!-- Bootstrap JS -->
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
-<form method="POST" id="member-form" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data" class="align-content-xl-center">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+    <!-- Data Tables -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+    <!-- Font Google -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+    <!-- Own CSS -->
+    <link rel="stylesheet" href="css/style.css">
+<form method="post" enctype="multipart/form-data" class="align-content-xl-center">
 <!---ATAS-->
 <?php include_once '../navbar.php'; ?>
 <div class="form-row ml-5 mt-1 justify-content-center">
     <div class="col-md-2 mb-3 my-1">
       <label for="nama">Nama :</label>
-      <input type="text" class="form-control" id="nama" name="nama"  placeholder="Masukan nama...">
+      <input type="text" class="form-control" id="nama" name="nama" required placeholder="Masukan nama...">
     </div>
 	  <div class="col-md-2 mb-3 my-1 ml-2">
       <label for="username">Username :</label>
-      <input type="text" class="form-control" id="username" placeholder="Nama didalam game..." name="username" >
+      <input type="text" class="form-control" id="username" placeholder="Nama didalam game..." name="username" required>
     </div>
     <div class="col-md-2 mb-3 my-1 ml-2">
       <label for="discord">Discrod tag :</label>
-      <input type="text" class="form-control" id="discord" placeholder="Velskude#6969" name="discord" >
+      <input type="text" class="form-control" id="discord" placeholder="Velskude#6969" name="discord" required>
     </div>
   </div>
 <!-- Tengah -->
 <div class="form-row ml-5 mt-1 justify-content-center">
     <div class="col-md-2 mb-3 my-1">
       <label for="asal">Asal :</label>
-      <input type="text" class="form-control" id="asal" placeholder="Masukan asal daerah..." name="asal" >
+      <input type="text" class="form-control" id="asal" placeholder="Masukan asal daerah..." name="asal" required>
     </div>
     <div class="col-md-2 mb-3 my-1 ml-2">
       <label for="umur">Umur :</label>
-      <input type="number" class="form-control" id="umur" placeholder="Masukan Umur..." name="umur" >
+      <input type="number" class="form-control" id="umur" placeholder="Masukan Umur..." name="umur" required>
     </div>
     <div class="col-md-2 mb-3 my-1 ml-2">
       <label for="hobi">Hobi :</label>
-      <input type="text" class="form-control" id="hobi" placeholder="Masukan Hobi..." name="hobi" >
+      <input type="text" class="form-control" id="hobi" placeholder="Masukan Hobi..." name="hobi" required>
     </div>
   </div>
 <!-- Bawah -->
@@ -192,40 +176,7 @@ $conn->close();
     </div>
 
   <div class="form-group col-md-1 ml-5 mt-4">
-<button type="submit" class="btn btn-primary" name="submit">Upload</button>
-  </form>
-
-  <!-- Elemen pesan -->
-  <div id="message" class="alert" style="position: fixed; top: 20px; right: 20px; display: none;"></div>
-
-  <!-- Script untuk menampilkan pesan fade in dan fade out -->
-  <script>
-    $(document).ready(function() {
-      // Fade in pesan
-      function showSuccessMessage(message) {
-        $('#message').removeClass('alert-danger').addClass('alert-success').text(message).fadeIn().delay(3000).fadeOut();
-      }
-
-      function showErrorMessage(message) {
-        $('#message').removeClass('alert-success').addClass('alert-danger').text(message).fadeIn().delay(3000).fadeOut();
-      }
-
-      // Validasi file size dan format
-      $('#photo').change(function() {
-        var file = this.files[0];
-        var fileSize = file.size;
-        var fileType = file.type;
-        var validFormats = ['image/jpeg', 'image/jpg', 'image/png'];
-
-        if (fileSize > 500000) {
-          showErrorMessage('Sorry, your file is too large.');
-          $(this).val('');
-        } else if (!validFormats.includes(fileType)) {
-          showErrorMessage('Sorry, only JPG, JPEG, and PNG files are allowed.');
-          $(this).val('');
-        }
-      });
-    });
-  </script>
-</body>
-</html>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</div>
+</div>
+</form>
